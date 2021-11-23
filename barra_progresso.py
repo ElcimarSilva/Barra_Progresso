e6baa8a0f1461cdb2e04af231d509401504aa720
ElcimarSilva <elcimar8@gmail.com>
2021-11-23 19:26:05 -0300

    first commit

---
+++ barra_progresso.py
@@ -0,0 +1,17 @@
+import time
+
+from tqdm import tqdm
+# # forma mais simples
+# for i in tqdm(range(10)):
+#     time.sleep(1)
+
+# lista = [1, 2, 3, 4, 5, 6]
+# for item in tqdm(lista):
+#     time.sleep(1)
+
+
+# with tqdm(total=100) as barra_progresso:
+#     for i in range(10):
+#         time.sleep(1)
+#         barra_progresso.update(10)
+
---
+++ readme.md
@@ -0,0 +1,6 @@
+Video
+https://www.youtube.com/watch?v=qRFPGuBc-KE
+tqdm
+
+ Rever a parte do requests
+ 
